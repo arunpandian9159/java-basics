bfa39c1ba8b0ca28096edeaf2c9fd0a40395e5b6
rocky <rocky@Arunpandian>
2024-06-27 11:03:02 +0530

    basic java project

---
+++ java_basics/src/java_basics/Basics.java
@@ -0,0 +1,12 @@
+package java_basics;
+
+public class Basics {
+
+	public static void main(String[] args) {
+	System.out.println("my name is arun");
+	System.out.println("MVIT");
+	System.out.print("TNSIF ");
+
+	}
+
+}
