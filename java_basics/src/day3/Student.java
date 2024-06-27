50ef5b8ef5a56f7d4cf7c83c8cd410992fcd2377
rocky <rocky@Arunpandian.mshome.net>
2024-06-27 15:36:56 +0530

    changes

--- java_basics/src/day3/Student.java
+++ java_basics/src/day3/Student.java
@@ -1,8 +1,30 @@
 package day3;
 
 public class Student {
-	int sid;
-	int m;
-	String sname;
-	String city;
+	private int sid;
+	private String sname;
+	private String city;
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", city=" + city + "]";
+	}
+	
 }
--- java_basics/src/day3/StudentDemo.java
+++ java_basics/src/day3/StudentDemo.java
@@ -3,10 +3,24 @@ package day3;
 public class StudentDemo {
 
 	public static void main(String[] args) {
+		
 		Student s1 = new Student();
-		System.out.println("Student id:"+s1.sid);
-		System.out.println("Student name:"+s1.sname);
-		System.out.println("Student city:"+s1.city);
+		s1.setSid(102);
+		s1.setCity("pondy");
+		s1.setSname("arun");
+		System.out.println(s1);
+		
+		Student s2 = new Student();
+		s2.setSid(103);
+		s2.setCity("chennai");
+		s2.setSname("justin");
+		System.out.println(s2);
+		
+		Student s3 = new Student();
+		s3.setSid(104);
+		s3.setCity("madurai");
+		s3.setSname("sathish");
+		System.out.println(s3);
 
 	}
 
