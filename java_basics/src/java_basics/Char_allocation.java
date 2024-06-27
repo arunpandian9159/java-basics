c2e085e11ffc0ab02540b284026e41a566aa3808
rocky <rocky@Arunpandian.mshome.net>
2024-06-27 13:21:14 +0530

    character allocation and data type demo

---
+++ java_basics/src/java_basics/Char_allocation.java
@@ -0,0 +1,23 @@
+package java_basics;
+
+public class Char_allocation {
+
+	public static void main(String[] args) {
+		char ch='a';
+		System.out.println(ch);
+		
+		char ch1=65;
+		System.out.println(ch1);
+		
+		char var='\u00A7';
+		System.out.println(var);
+		
+		char var1='\u20AC';
+		System.out.println(var1);
+		
+		int a = 'A';
+		System.out.println(a);
+
+	}
+
+}
